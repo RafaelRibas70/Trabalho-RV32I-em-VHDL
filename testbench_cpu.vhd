c7fc43e05bd8d87fdf09f78b64d1005c655f54a1
EduardoReichertB <eduardobasanezreichert@gmail.com>
2025-07-02 03:01:11 -0300

    Update testbench_cpu.vhd

--- testbench_cpu.vhd
+++ testbench_cpu.vhd
@@ -2,41 +2,28 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity testbench_cpu is
-end testbench_cpu;
-
-architecture teste of testbench_cpu is
-
-component cpu is
-port(
-  i_CLK : in std_logic,
-  i_RST : in std_logic
-);
-end component;
-
-signal w_CLK, w_RST : std_logic;
-
-begin
-
-macaco : cpu
-port map(
-  i_CLK => w_CLK,
-  i_RST => w_RST
-);
+entity pc is
+  port (
+    i_CLK  : in std_logic; -- clock
+	 i_RST  : in std_logic; --reset
+    i_Data : in std_logic_vector (31 downto 0); 
+    o_PC   : out std_logic_vector (31 downto 0) 
+  ); 
+end pc; 
   
-process
+architecture arch_pc of pc is
 begin
-    w_RST <= '1';
-	 w_CLK <= '0';
-	 w_RST <= '0';
-	 w_CLK <= '1';
+
+  process(i_CLK, i_RST) 
+  begin
+  
+    if (rising_edge(i_CLK)) then
+	if (i_RST = '1')then
+	  o_PC <= "00000000000000000000000000000000";
+	end if;
+	o_PC <= i_Data;
+    end if;
 	 
-    for i in 1 to 47 loop
-        w_CLK <= '0';
-        wait for 1000 ns;
-        w_CLK <= '1';
-        wait for 1000 ns;
-    end loop;
-    wait; 
-end process;
-end;
+  end process;
+  
+end arch_pc;
