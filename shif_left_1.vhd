4ee0edea47e3c7cde22bf58e5a72a2d72bd60f54
RafaelRibas70 <rafaelBurattoRibas70@gmail.com>
2025-07-02 00:48:32 -0300

    Update shif_left_1.vhd

--- shif_left_1.vhd
+++ shif_left_1.vhd
@@ -4,8 +4,8 @@ use ieee.numeric_std.all;
 
 entity shif_left_1 is
     port (
-        i_entrada_imediato  : in  std_logic_vector(31 downto 0);
-	o_saida_imediato    : out std_logic_vector(31 downto 0)
+      i_entrada_imediato  : in  std_logic_vector(31 downto 0);
+      o_saida_imediato    : out std_logic_vector(31 downto 0)
     );
 end entity;
 	
