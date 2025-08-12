f3ea09c106cea70935fc787665859e61ba8ba0e9
EduardoReichertB <eduardobasanezreichert@gmail.com>
2025-08-12 15:05:52 -0300

    Update ULA.vhd

--- ULA.vhd
+++ ULA.vhd
@@ -64,15 +64,14 @@ end component;
 component mux32_8x1 is
 port (
  i_SEL  : in std_logic_vector (2 downto 0);
- i_A    : in std_logic_vector (31 downto 0);  --ADD 
- i_B    : in std_logic_vector (31 downto 0);  --SUB 
- i_C    : in std_logic_vector (31 downto 0);  --AND 
- i_D    : in std_logic_vector (31 downto 0);  --OR
- i_E    : in std_logic_vector (31 downto 0);  --XOR
- i_F    : in std_logic_vector (31 downto 0);  --SLT
- i_G    : in std_logic_vector (31 downto 0);
- i_H    : in std_logic_vector (31 downto 0);
- i_I    : in std_logic_vector (31 downto 0);
+ i_A    : in std_logic_vector (31 downto 0);  --ADD 000
+ i_B    : in std_logic_vector (31 downto 0);  --SUB 001
+ i_C    : in std_logic_vector (31 downto 0);  --AND 010
+ i_D    : in std_logic_vector (31 downto 0);  --OR  011
+ i_E    : in std_logic_vector (31 downto 0);  --XOR 100
+ i_F    : in std_logic_vector (31 downto 0);  --SLT 101
+ i_G    : in std_logic_vector (31 downto 0);  --SLL 110
+ i_H    : in std_logic_vector (31 downto 0);  --SRL 111
  o_S    : out std_logic_vector(31 downto 0)
 );
 end component;
@@ -161,3 +160,4 @@ port map(
 );
 
 end architecture;
+
